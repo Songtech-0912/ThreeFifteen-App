4c259021f5faf2db4957e04536c93d697cb63d52
Jacky Song <almey0721@gmail.com>
2024-07-01 09:00:16 +0000

    Refactored app.py to function exclusively as server
    Threefifteen now hosts its stylesheet and client/admin UIs
    on GitHub pages, there is no need to serve the clients on
    the Flask server, so app.py is now just a server, no longer
    acting as a server and rendering the web UI for the clients

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS, cross_origin
-import sys
-import json
-from time import sleep
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
 original_busdata = {
-    "present": [],
-    "absent": [i for i in range(1, 32)]
+    "absent": [i for i in range(1, 32)],
+    "left_street": [],
+    "right_street": [],
+    "left_school": [],
+    "right_school": []
 }
 
 busdata = original_busdata
+
+def generatepwd():
+    token = "5fc0ac76f035d603d525d21ecad5d80c"
+    pwd = token[3] + token[5] + token[1] + token[-1] + token[8] + token[9] + token[2] + token[-2] + token[7] + token[7] + token[11]
+    return pwd
+
+password = {
+    "key": generatepwd()
+}
 
 @app.route("/")
 def main():
     global busdata
     return render_template("main.html", busdata=busdata)
+
+@app.route("/key", methods=["GET"])
+@cross_origin()
+def get_key():
+  global password
+  return password
 
 @app.route("/busdata", methods=["GET", "POST"])
 @cross_origin()
@@ -28,31 +43,6 @@ def transfer_data():
       # probably add in a signed integrity check here
       busdata = request.get_json()
     return jsonify(busdata)
-
-@app.route("/admin", methods=["GET", "POST"])
-def template():
-    global busdata
-    present = []
-    absent = []
-    # When the form is submitted, adjust
-    # busdata correspondingly
-    if request.method == "POST":
-        for i in range(1, 32):
-            if f"bus-{i}" in request.form:
-                present.append(i)
-            else:
-                absent.append(i)
-        busdata["present"] = present
-        busdata["absent"] = absent
-    # Assemble checkbox data from busdata
-    checkbox_data = {}
-    for bus in busdata["present"]:
-        checkbox_data[bus] = False
-    for bus in busdata["absent"]:
-        checkbox_data[bus] = True
-    # Sort checkbox data so it is in the right order
-    checkbox_data = dict(sorted(checkbox_data.items()))
-    return render_template("admin.html", busdata=busdata, checkbox_data=checkbox_data)
 
 if __name__ == "__main__":
     app.run()
